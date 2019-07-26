a2604cd7f4cc4321ffb8d17dedb58d19de07bb89
marshallprotsman <47481879+MarshallProtsman@users.noreply.github.com>
2019-07-25 20:15:52 -0400

    restructured files

--- rp-19-react/src/components/grid/Grid.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./gridstyle.css";
-
-function Grid (photo) {
-    return <div className = "grid card" >{photo.children}</div>
-}
-
-export default Grid;
--- rp-19-react/src/test.js
+++
@@ -1,7 +0,0 @@
-const imagesJSON = "./images.json"
-
-imagesJSON.map(imageArr) => {
-console.log(imagesArr);
-}
-
-console.log(imagesArr)
